ce2741017069d5d2bdbda98433d88290b7ba0eb1
blue <d.zanella@escola.pr.gov.br>
2025-06-24 10:59:02 +0000

    commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const BotaoPalavras = document.querySelector(#botao-keyword)
-BotaoPalavras.addeventlistener("click, mostraPalavras")
-function mostraPalavras() {
-    alert("Fui clicado!");
+const BotaoPalavras = document.querySelector('#botao-palavra');
+
+BotaoPalavras.addEventListener('click', mostraPalavras); 
+
+function mostraPalavras() { 
+    alert('fui clicado!');
 }
