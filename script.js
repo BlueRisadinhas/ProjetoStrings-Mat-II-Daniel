68e7b7787d0524e5da7f61fe2a31902994669a17
blue <d.zanella@escola.pr.gov.br>
2025-06-24 11:27:44 +0000

    commit

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const BotaoPalavras = document.querySelector('#botao-palavra');
 
 BotaoPalavras.addEventListener('click', mostraPalavras); 
 
-function mostraPalavras() { 
-    alert('fui clicado!');
+function mostraPalavras() {
+    const texto = document.querySelector('#entrada-texto').value;
+
+    const CampoResult = document.querySelector('#resultado-palavra');
+    
+    const palavras = texto.split(" "); 
+
+     CampoResult.textContent = palavras.join(", ");
 }
